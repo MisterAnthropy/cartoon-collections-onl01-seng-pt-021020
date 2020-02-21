be82f33263b8b0d185b7a34dfc4d6a3670c71ed6
excellent-revision-4527 <tjcroley@outlook.com>
2020-02-21 03:26:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |n| n}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(short_words)
+  short_words.any? { |i| i > 4 }
 end
 
 def find_the_cheese# code an argument here
